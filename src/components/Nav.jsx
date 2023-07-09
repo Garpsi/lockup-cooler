db73e764120e9be6c14988babe5698a952305234
Garpsi Ho <garpsi@Garpsis-MacBook-Pro.local>
2023-07-09 17:10:13 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav__container">
+        <div className="row">
+          
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
